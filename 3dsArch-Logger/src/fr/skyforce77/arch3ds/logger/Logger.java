277e4c2059a4d3cd50ba77a8f8321bd508c7850a
Skyforce77 <arnaud.bruyer@gmail.com>
2015-03-20 07:17:30 +0100

    Added primary files

---
+++ 3dsArch-Logger/src/fr/skyforce77/arch3ds/logger/Logger.java
@@ -0,0 +1,62 @@
+package fr.skyforce77.arch3ds.logger;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import fr.skyforce77.arch3ds.api.Plugin;
+import fr.skyforce77.arch3ds.api.graphics.ArchGraphics;
+import fr.skyforce77.arch3ds.api.graphics.ArchScreen;
+import fr.skyforce77.arch3ds.api.input.ArchAxis;
+import fr.skyforce77.arch3ds.api.input.ArchInput;
+
+public class Logger extends Plugin{
+
+	@Override
+	public String getName() {
+		return "Logger";
+	}
+
+	@Override
+	public String getVersion() {
+		return "0.1";
+	}
+
+	@Override
+	public void onInit() {
+		System.out.println("[Logger] Called init event");
+	}
+	
+	@Override
+	public void onEnable() {
+		System.out.println("[Logger] Called enable event");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("[Logger] Called disable event");
+	}
+
+	@Override
+	public void onInput(ArchInput input, byte status) {
+		System.out.println("[Logger] Called input event: ["+input.getCode()+": "+status+"]");
+	}
+
+	@Override
+	public void onAxis(ArchAxis axis, double location) {
+		System.out.println("[Logger] Called axis event: ["+axis.getCode()+": "+location+"]");
+	}
+
+	@Override
+	public void drawScreen(ArchGraphics graphics) {
+		Graphics2D g2d = graphics.getGraphics();
+		g2d.clearRect(0, 0, graphics.getWidth(), graphics.getHeight());
+		g2d.setColor(Color.BLACK);
+		g2d.fillRect(0, 0, graphics.getWidth(), graphics.getHeight());
+		
+		if(graphics.getScreen().equals(ArchScreen.TOP_SCREEN)) {
+			g2d.setColor(Color.WHITE);
+			g2d.drawString("Logger", 0, graphics.getHeight()/2);
+		}
+	}
+
+}
